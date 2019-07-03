730823fc7812b68b0d73c1543ee7f816c85608f6
anna yovandich <anna.yovandich@ff0000.com>
2019-07-03 13:52:40 -0700

    Add dynamic SNS subscription

--- src/fetchSend/index.js
+++ src/fetchSend/index.js
@@ -1,6 +1,27 @@
+const AWS = require('aws-sdk');
+const sns = new AWS.SNS();
 exports.handler = async (event, context) => {
-  // Log the event argument for debugging and for use in local development.
-  console.log(JSON.stringify(event, undefined, 2));
+  const topicArn = process.env.TOPIC_ARN;
+  const endpoint = 'anna@stackery.io';
 
-  return {};
+  const stopId = 7747;
+  const route = 8;
+
+  const subscriptionsResults = await sns.listSubscriptionsByTopic({ TopicArn: topicArn }).promise();
+  const subscriptions = subscriptionsResults.Subscriptions;
+
+  if (!subscriptions.find(subscription => subscription.Endpoint === endpoint)) {
+    await sns.subscribe({
+      Protocol: 'email',
+      TopicArn: topicArn,
+      Endpoint: endpoint,
+      Attributes: {
+        FilterPolicy: JSON.stringify({
+          stopId: [stopId],
+          route: [route],
+          isScheduledTime: [true]
+        })
+      }
+    }).promise();
+  }
 };
